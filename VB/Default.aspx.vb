aeca56c58bf52ac5eb3c81b9ec7979380de8c034
Oleg Kozlov <oleg.kozlov@devexpress.com>
2021-06-24 18:13:36 +0300

    Convert WebSite to WebApplication

--- VB/Default.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿
-Partial Class _Default
-    Inherits System.Web.UI.Page
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-
-    End Sub
-End Class
---
+++ VB/WebApplication1/DefaultForm.aspx.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web
+
+Namespace WebApplication1
+	Partial Public Class DefaultForm
+		Inherits System.Web.UI.Page
+		Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+			' Creates a column, customizes its settings and appends it to the Columns collection;
+		End Sub
+
+		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+		End Sub
+	End Class
+End Namespace
