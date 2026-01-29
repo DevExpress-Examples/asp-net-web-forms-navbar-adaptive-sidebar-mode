7121c157707ddb5cf290565f66dcdbafdafc226a
DevExpressExampleBot <fakeEmail>
2026-01-29 20:28:04 +0400

    Source auto update [skip ci]

---
+++ VB/WebApplication1/Default.aspx.vb
@@ -0,0 +1,21 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web
+
+Namespace WebApplication1
+	Partial Public Class [Default]
+		Inherits System.Web.UI.Page
+
+		Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+			' Creates a column, customizes its settings and appends it to the Columns collection;
+		End Sub
+
+		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+		End Sub
+	End Class
+End Namespace
